6880ec3c5e3fb93aeee6ac38833b385d7781adfa
Geraniel <geraniel.motta@monitoratec.com.br>
2023-05-25 15:16:11 -0300

    Configurando aplicação para deploy

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,13 @@ import { env } from './env'
 import { knex as setupKnex, Knex as knexConf } from 'knex'
 
 export const config: knexConf.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
